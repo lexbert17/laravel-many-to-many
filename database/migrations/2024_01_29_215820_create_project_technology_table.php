8b7da366609d7d1846cff2fbc10eab2381abdc42
lexbert17 <lexbertlazo@gmail.com>
2024-01-29 22:59:11 +0100

    refactoring: rinominazione seeder

--- database/migrations/2024_01_29_215820_create_project_technology_table.php
+++ database/migrations/2024_01_29_215820_create_project_technology_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('project_tecnology', function (Blueprint $table) {
+        Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('project_tecnology');
+        Schema::dropIfExists('project_technology');
     }
 };
