60a97c1ca8541a482d285a77b5a98350135cff7a
lexbert17 <lexbertlazo@gmail.com>
2024-01-29 21:10:23 +0100

    add: inserimento campi nella tabella technologies

--- database/migrations/2024_01_29_200157_create_technologies_table.php
+++ database/migrations/2024_01_29_200157_create_technologies_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('hex_color')->nullable();
             $table->timestamps();
         });
     }
